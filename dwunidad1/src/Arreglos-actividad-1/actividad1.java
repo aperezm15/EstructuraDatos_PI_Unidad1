fc7efdb2102a08665b3e618eef884ab5191883e5
Alvaro Perez <265096469+mitzdev1999@users.noreply.github.com>
2026-03-20 14:07:10 -0500

    feat(actividad1): Arreglos>Declararion y creacion de un arreglo>Declarar un arreglo de 10 enteros en cada lenguaje dado.

---
+++ dwunidad1/src/Arreglos-actividad-1/actividad1.java
@@ -0,0 +1,15 @@
+import java.util.Random;
+
+public class actividad1 {
+    public static void main(String[] args) {
+        //Creamos un array de 10 enteros, teniendo en cuenta el siguiente punto que deben
+        //inicializarce aleatoriamente.
+        int[] numeros = new int[10];
+
+        
+        //Utilizamos Random para crear numeros aleatorios, importamos libreria
+        Random random = new Random();
+
+    }
+    
+}
