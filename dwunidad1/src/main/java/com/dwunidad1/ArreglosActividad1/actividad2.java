cd3ffdee9e6700dbf03df7aec626cda669dde753
Alvaro Perez <265096469+mitzdev1999@users.noreply.github.com>
2026-04-07 17:19:19 -0500

    feat(matrices): Actualizacion, los cambios no se suibieron al repo ya que no se guardo el archivo local

---
+++ dwunidad1/src/main/java/com/dwunidad1/ArreglosActividad1/actividad2.java
@@ -0,0 +1,17 @@
+package com.dwunidad1.ArreglosActividad1;
+
+public class actividad2 {
+    public static void main(String[] args){
+        System.out.println("\n-------------------------");
+            System.out.println("Punto 1, Declaracion e inicializacion");
+            System.out.println("-------------------------");
+
+            //Creamos una matriz 3x3 con valores de 1 al 9
+            int[][] matriz = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+            };
+    }
+     
+}
